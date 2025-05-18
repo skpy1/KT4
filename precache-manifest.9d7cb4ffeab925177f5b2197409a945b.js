c124af2659cca103ed61022580d0d49420a365cc
Клим Щуров <klimsurov@MacBook-Air-Klim.local>
2025-05-18 20:36:44 +0300

    Updates

---
+++ precache-manifest.9d7cb4ffeab925177f5b2197409a945b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceafc36bc0fd8d40d96f10ebb4be4081",
+    "url": "/KT4/index.html"
+  },
+  {
+    "revision": "666b2c6825b8c01f3b15",
+    "url": "/KT4/static/css/main.04773874.chunk.css"
+  },
+  {
+    "revision": "edbcc5a7f809eb999064",
+    "url": "/KT4/static/js/2.d40a4d68.chunk.js"
+  },
+  {
+    "revision": "237983766bae5aafcdb46e18c466205c",
+    "url": "/KT4/static/js/2.d40a4d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "666b2c6825b8c01f3b15",
+    "url": "/KT4/static/js/main.799e38ff.chunk.js"
+  },
+  {
+    "revision": "311a412f6565b0663873",
+    "url": "/KT4/static/js/runtime-main.ddb574a5.js"
+  }
+]);
